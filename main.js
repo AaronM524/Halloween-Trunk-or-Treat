6672fb1589e54a6fb22814dfd711bb75189d1d17
AaronM524 <aaronmathew468@gmail.com>
2024-10-15 23:35:21 -0400

    done

--- main.js
+++ main.js
@@ -15,9 +15,20 @@ $(document).ready(function() {
 
 $(document).ready(function () {
     $('.card').on('mouseenter', function () {
-        $(this).find('.description').stop(true, true).fadeIn(300);
+        $(this).find('.description').stop(true, true).fadeIn(300); //looks for child element of the class description and stops any ongoing animations 
     }).on('mouseleave', function () {
         $(this).find('.description').stop(true, true).fadeOut(300);
+    });
+});
+
+
+$(document).ready(function() {
+    $('.event-heading').on('click', function() {
+        // Get the target ID from the button's data attribute
+        var targetId = $(this).data('target');
+        
+        //finds the element with the ID that matches the targetID, and it toggles its visibility
+        $('#' + targetId).slideToggle();
     });
 });
 
